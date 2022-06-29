66162e87f3f1e43d7f834b76dd74d2b8ad3d7b0a
balazsiisti <istvan.balazsi@ibm.com>
2022-06-29 13:55:26 +0300

    Home page WIP

---
+++ data/projectList.ts
@@ -0,0 +1,24 @@
+const projectList = [
+  {
+    name: "Flip Clock",
+    status: 'In progress',
+    route: '/clock',
+  },
+  {
+    name: "Timer",
+    status: 'Not Started',
+    route: '/timer',
+  },
+  {
+    name: "To Do",
+    status: 'Not Started',
+    route: '/to-do',
+  },
+  {
+    name: "Spend Money",
+    status: 'Not Started',
+    route: '/spend-money',
+  },
+]
+
+export { projectList };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
+import { useState } from 'react';
 import type { NextPage } from 'next'
+import { projectList } from '../data/projectList'
 
 const Home: NextPage = () => {
+  const [projects] = useState(projectList);
   return (
-    <div className='bg-slate-600'>
-    kjkkjnkjn
+    <div className='bg-gray-700 h-screen w-screen text-slate-200'>
+      <h1 className='text-center text-3xl p-6'>Hi👋<br/> I&apos;m <b>Isti</b> and welcome on my totally incomplete page</h1>
+      <div>
+        <p className='text-center text-xl pt-5'>My mainly goal in this page to create and well document🤯 web applications</p>
+        <div>
+          {projects.map(project => (
+            <ul key={project.name}>
+              <li>
+                {project.name}
+              </li>
+            </ul>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
